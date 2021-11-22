c5d58816f0dca91c84b3212b502f6a0fc1a9bb18
Alexandra Weaver <aweaver0124@gmail.com>
2021-11-22 13:22:39 -0600

    commit tree b85a12f8f92401d417a54c4f3c2cf1b28e06f306

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Write your code in this file!
+const currentUser = "Alexandra";
+
+let welcomeMessage = "Welcome to Flatbook, ";
+
+welcomeMessage = `Welcome to Flatbook, ${currentUser}!`
+
+let excitedWelcomeMessage = "WELCOME TO FLATBOOK, ";
+
+const upperCaseCurrentUser = "ALEXANDRA"
+excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${upperCaseCurrentUser}!`
+
+let shortGreeting = "Welcome, ";
+
+const firstInitial = currentUser[0];
+const restOfName = currentUser.slice(1);
+
+shortGreeting = `Welcome, ${firstInitial}!`
